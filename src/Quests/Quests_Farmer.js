bf4e9f206ee8b685494e42d0724914746c87f713
Jake Swartwout <jacob.swartwout@gmail.com>
2023-04-09 01:02:39 -0400

    Making dialogue state machine

--- src/Quests/Quests_Farmer.js
+++ src/Quests/Quests_Farmer.js
@@ -8,72 +8,92 @@ let five_carrots = {
 }
 const Q_GATHER_5_CARROTS = CLAIM_QUEST_ID()
 
+const farmer_convo = {
+    NOT_MET: 0,
+    FIRST_MEETING: 1,
+    WAITING_ON_CROPS: 2,
+    TURN_IN: 3,
+    NEXT_STEPS: 4,
+}
+
 
 /********************* Farmer Class *********************/
 
 class Quests_Farmer extends QuestStates {
     constructor() {
         super([Q_GATHER_5_CARROTS])
-        this.num_meetings = 0
-        this.has_enough_carrots = false
-        this.got_final_dialogue = false
+        this.convo_stage = farmer_convo.NOT_MET
     }
     update() {
-        this.num_meetings += 1
-        // only update if they're missing the quest
-        if(!this.has_enough_carrots) {
-            this.has_enough_carrots = PLAYER.inventory.contains(five_carrots)
-            if (this.has_enough_carrots) {
-                PLAYER.inventory.remove(five_carrots)
-                this.finish(Q_GATHER_5_CARROTS)
-            }
-        } else {
-            // log if they've gotten the completion dialogue or not
-            this.got_final_dialogue = true
+        switch(this.convo_stage) {
+            case farmer_convo.NOT_MET:
+                this.convo_stage = farmer_convo.FIRST_MEETING
+                break;
+            case farmer_convo.FIRST_MEETING:
+            case farmer_convo.WAITING_ON_CROPS:
+                if (PLAYER.inventory.contains(five_carrots)) {
+                    this.convo_stage = farmer_convo.TURN_IN
+                    PLAYER.inventory.remove(five_carrots)
+                    this.finish(Q_GATHER_5_CARROTS)
+                } else {
+                    this.convo_stage = farmer_convo.WAITING_ON_CROPS
+                }
+                break;
+            case farmer_convo.TURN_IN:
+                this.convo_stage = farmer_convo.NEXT_STEPS
+                break;
+            case farmer_convo.NEXT_STEPS:
+            default:
+                break;
         }
     }
     getDialogue() {
-        if (this.num_meetings == 1) {
-            return new TextBox([
-                new Speech("Farmer", [
-                    "Hello traveler! Nice to have a visitor here.",
-                    "You're passing through my carrot farm! Pretty impressive, right?",
-                ]),
-                new Speech(PLAYER_NAME, [
-                    "Yes! I love it",
-                ]),
-                new Speech("Farmer", [
-                    "Aw thank you!",
-                    "We're getting ready for dinner, if you're hungry you're welcome to join.",
-                    "I'm out here to get some vegetables,",
-                    "I'll need 5 carrots to take you back.",
-                ]),
-                new Speech(PLAYER_NAME, [
-                    "Got it! I'll grab some for you!",
-                ]),
-            ])
-        } else if (!this.has_enough_carrots) {
-            return new TextBox([
-                new Speech("Farmer", [
-                    "Please, I need at least 5 carrots for dinner tonight."
+        switch(this.convo_stage) {
+            case farmer_convo.NOT_MET:
+                return;
+            case farmer_convo.FIRST_MEETING:
+                return new TextBox([
+                    new Speech("Farmer", [
+                        "Hello traveler! Nice to have a visitor here.",
+                        "You're passing through my carrot farm! Pretty impressive, right?",
+                    ]),
+                    new Speech(PLAYER_NAME, [
+                        "Yes! I love it",
+                    ]),
+                    new Speech("Farmer", [
+                        "Aw thank you!",
+                        "We're getting ready for dinner, if you're hungry you're welcome to join.",
+                        "I'm out here to get some vegetables,",
+                        "I'll need 5 carrots to take you back.",
+                    ]),
+                    new Speech(PLAYER_NAME, [
+                        "Got it! I'll grab some for you!",
+                    ]),
                 ])
-            ])
-        } else if (!this.got_final_dialogue) {
-            return new TextBox([
-                new Speech(PLAYER_NAME, [
-                    "I have 5 carrots!",
-                ]),
-                new Speech("Farmer", [
-                    "Thanks!",
-                    "I'm going to do some weeding and will meet you at the house."
+            case farmer_convo.WAITING_ON_CROPS:
+                return new TextBox([
+                    new Speech("Farmer", [
+                        "Please, I need at least 5 carrots for dinner tonight."
+                    ])
                 ])
-            ])
-        } else {
-            return new TextBox([
-                new Speech("Farmer", [
-                    "Just keep following this path and you'll reach the house!"
+            case farmer_convo.TURN_IN:
+                return new TextBox([
+                    new Speech(PLAYER_NAME, [
+                        "I have 5 carrots!",
+                    ]),
+                    new Speech("Farmer", [
+                        "Thanks!",
+                        "I'm going to do some weeding and will meet you at the house."
+                    ])
                 ])
-            ])
+            case farmer_convo.NEXT_STEPS:
+                return new TextBox([
+                    new Speech("Farmer", [
+                        "Just keep following this path and you'll reach the house!"
+                    ])
+                ])
+            default:
+                return;
         }
     }
 }
--- src/Quests/Quests_FarmersWife.js
+++ src/Quests/Quests_FarmersWife.js
@@ -13,6 +13,14 @@ for (let crop in CROPS) {
 }
 const Q_GATHER_7_PETRAS = CLAIM_QUEST_ID()
 
+const farmer_convo = {
+    NOT_MET: 0,
+    FIRST_MEETING: 1,
+    WAITING_ON_CROPS: 2,
+    TURN_IN: 3,
+    NEXT_STEPS: 4,
+}
+
 
 /********************* Farmer Class *********************/
 
@@ -20,65 +28,87 @@ const Q_GATHER_7_PETRAS = CLAIM_QUEST_ID()
 class Quests_FarmersWife extends QuestStates {
     constructor() {
         super([Q_GATHER_7_PETRAS])
-        this.num_meetings = 0;
-        this.has_enough_petras = false
+        this.convo_stage = farmer_convo.NOT_MET
     }
     update() {
-        this.num_meetings += 1
-        // only update if they're missing the quest
-        if(!this.has_enough_petras) {
-            this.has_enough_petras = PLAYER.inventory.contains(seven_petras)
-            if (this.has_enough_petras) {
-                PLAYER.inventory.remove(all_crops)
-                this.finish(Q_GATHER_7_PETRAS)
-            }
+        switch(this.convo_stage) {
+            case farmer_convo.NOT_MET:
+                this.convo_stage = farmer_convo.FIRST_MEETING
+                break;
+            case farmer_convo.FIRST_MEETING:
+            case farmer_convo.WAITING_ON_CROPS:
+                if (PLAYER.inventory.contains(seven_petras)) {
+                    this.convo_stage = farmer_convo.TURN_IN
+                    PLAYER.inventory.remove(all_crops)
+                    this.finish(Q_GATHER_7_PETRAS)
+                } else {
+                    this.convo_stage = farmer_convo.WAITING_ON_CROPS
+                }
+                break;
+            case farmer_convo.TURN_IN:
+                this.convo_stage = farmer_convo.NEXT_STEPS
+                break;
+            case farmer_convo.NEXT_STEPS:
+            default:
+                break;
         }
     }
     getDialogue() {
-        if (this.num_meetings == 1) {
-            return new TextBox([
-                new Speech(PLAYER_NAME, [
-                    "Hi do you know where the farmhouse is?",
-                    "I'm bringing some carrots for dinner!"
-                ]),
-                new Speech("Farmers Wife", [
-                    "Carrots?",
-                    "No no, I wanted to try the new crop for dinner!",
-                    "You must have met my husband then, he's scared of the new stuff."
-                ]),
-                new Speech(PLAYER_NAME, [
-                    "New crop?"
-                ]),
-                new Speech("Farmers Wife", [
-                    "Yeah! These new purple vegetables sprung up in our fields one day.",
-                    "Things have been a little crazy lately, so I guess even the plants are off now.",
-                    "I still want to try them, so would you pick some of those for me?",
-                    "If you'll be eating with us, that would need 7 or so."
-                ]),
-                new Speech(PLAYER_NAME, [
-                    "Sure!",
-                ]),
-            ])
-        } else if (!this.has_enough_petras) {
-            return new TextBox([
-                new Speech("Farmers Wife", [
-                    "Do you have 7 of those strange purple vegetables yet?"
-                ]),
-                new Speech(PLAYER_NAME, [
-                    "Not yet, I'll go pick a few."
-                ]),
-            ])
-        } else {
-            return new TextBox([
-                new Speech(PLAYER_NAME, [
-                    "These are some strange vegetables...",
-                ]),
-                new Speech("Farmers Wife", [
-                    "I'm sure they'll cook up fine!",
-                    "I'll take whatever carrots you have too, might as well add them.",
-                    "I'll meet you back at the farmhouse, just keep heading that same direction."
+        switch(this.convo_stage) {
+            case farmer_convo.NOT_MET:
+                return;
+            case farmer_convo.FIRST_MEETING:
+                return new TextBox([
+                    new Speech(PLAYER_NAME, [
+                        "Hi do you know where the farmhouse is?",
+                        "I'm bringing some carrots for dinner!"
+                    ]),
+                    new Speech("Farmers Wife", [
+                        "Carrots?",
+                        "No no, I wanted to try the new crop for dinner!",
+                        "You must have met my husband then, he's scared of the new stuff."
+                    ]),
+                    new Speech(PLAYER_NAME, [
+                        "New crop?"
+                    ]),
+                    new Speech("Farmers Wife", [
+                        "Yeah! These new purple vegetables sprung up in our fields one day.",
+                        "Things have been a little crazy lately, so I guess even the plants are off now.",
+                        "I still want to try them, so would you pick some of those for me?",
+                        "If you'll be eating with us, that would need 7 or so."
+                    ]),
+                    new Speech(PLAYER_NAME, [
+                        "Sure!",
+                    ]),
                 ])
-            ])
+            case farmer_convo.WAITING_ON_CROPS:
+                return new TextBox([
+                    new Speech("Farmers Wife", [
+                        "Do you have 7 of those strange purple vegetables yet?"
+                    ]),
+                    new Speech(PLAYER_NAME, [
+                        "Not yet, I'll go pick a few."
+                    ]),
+                ])
+            case farmer_convo.TURN_IN:
+                return new TextBox([
+                    new Speech(PLAYER_NAME, [
+                        "These are some strange vegetables...",
+                    ]),
+                    new Speech("Farmers Wife", [
+                        "I'm sure they'll cook up fine!",
+                        "I'll take whatever carrots you have too, might as well add them.",
+                        "I'll meet you back at the farmhouse, just keep heading that same direction."
+                    ])
+                ])
+            case farmer_convo.NEXT_STEPS:
+                return new TextBox([
+                    new Speech("Farmers Wife", [
+                        "Keep following this trail and I'll meet you at the farmhouse!"
+                    ])
+                ])
+            default:
+                break;
         }
     }
 }
