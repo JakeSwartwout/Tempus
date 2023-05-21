baaa24de5758fa370dbe55169c4ff97ab79d6117
Jake Swartwout <jacob.swartwout@gmail.com>
2023-05-21 01:42:54 -0400

    Drawing the tileset for the farmhouse

---
+++ TiledMaps/HouseTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="HouseTiles" tilewidth="16" tileheight="16" tilecount="32" columns="8">
+ <image source="TileArt/HouseTiles.png" width="128" height="64"/>
+</tileset>
